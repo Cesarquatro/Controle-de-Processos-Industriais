c67af8506c3f2b0a57de2bdd316d872bc43d5963
Cesar Augusto M. C. da Silva <caugustomendes653@gmail.com>
2024-10-25 22:44:41 -0300

    It's alive

--- Atividade Extra/teste.py
+++ Atividade Extra/teste.py
@@ -1,9 +1,18 @@
 import serial
+import time
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 # this port address is for the serial tx/rx pins on the GPIO header
 SERIAL_PORT = 'COM17'
 # be sure to set this to the same rate used on the Arduino
 SERIAL_RATE = 115200
 
+dataList = {"Tempo(s)"    :[], 
+            "Setpoint"    :[], 
+            "Valor Atual" :[],
+            "Erro"        :[],
+            "Saída PWM"   :[]}
+Keys = list(dataList.keys())
 
 def main():
     ser = serial.Serial(SERIAL_PORT, SERIAL_RATE, timeout=0.1)
@@ -12,7 +21,23 @@ def main():
         # sent using Serial.println() on the Arduino
         reading = ser.readline().decode('utf-8')
         # reading is a string...do whatever you want from here
-        print(reading, end='')
+        #print(reading, end='')
+        
+        try:
+            reading = [float(value) for value in reading.strip().split(',')]
+            print(len(reading))
+            for i in range(len(reading)):
+                dataList[Keys[i]].append(reading[i])
+        except:
+            #print("Error")
+            pass
+        for key in Keys:
+            #limit the list to 50 elements
+            dataList[key] = dataList[key][-50:]
+            #print(f"{key}: {dataList[key]}")
+        #dataList = dataList[-50:]
+        #print("dataList")
+        #print(dataList)
 
 
 if __name__ == "__main__":
